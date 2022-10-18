4c165ee9981d567ce9ef681ce05eb9faf9122bf9
Sameer Varshney <105305958+sameerddr@users.noreply.github.com>
2022-10-18 21:06:12 +0530

    news-app

--- news-app/src/Components/Header/Navbar.jsx
+++ news-app/src/Components/Header/Navbar.jsx
@@ -22,7 +22,9 @@ function Navbar(props) {
         <li>
           <Link to="/about">About</Link>
         </li>
-        <li>{/* <Link to="/contact">Contact</Link> */}</li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
         <li>
           <Link to="/services">Services</Link>
         </li>
